72de93ffac527c2ecad27f8605f714dbaf4d18ed
GlobandX <bwyshthaitham@gmail.com>
2026-04-13 14:19:58 -1100

    Switched to manual persistence for 26.1.2 compatibility and stability

--- src/main/java/org/gbxteam/physis/ForestGrowthData.java
+++ src/main/java/org/gbxteam/physis/ForestGrowthData.java
@@ -2,31 +2,36 @@ package org.gbxteam.physis;
 
 //#if MC >= 260100
 //$$ import net.minecraft.core.BlockPos;
-//$$ import net.minecraft.core.HolderLookup;
 //$$ import net.minecraft.nbt.CompoundTag;
+//$$ import net.minecraft.nbt.NbtIo;
 //$$ import net.minecraft.server.level.ServerLevel;
-//$$ import net.minecraft.world.level.saveddata.SavedData;
+//$$ import net.minecraft.world.level.storage.LevelResource;
+//$$ import java.io.File;
+//$$ import java.io.IOException;
 //$$ import java.util.HashSet;
 //$$ import java.util.Set;
+//$$ import java.util.Map;
+//$$ import java.util.HashMap;
 
-//$$ public class ForestGrowthData extends SavedData {
+//$$ public class ForestGrowthData {
+//$$    private static final Map<ServerLevel, ForestGrowthData> instances = new HashMap<>();
 //$$    private final Set<Long> modPlantedSaplings = new HashSet<>();
+//$$    private final File saveFile;
 //$$
-//$$    public static ForestGrowthData get(ServerLevel level) {
-//$$        // Support for newer SavedData factory pattern in 26.1x
-//$$        SavedData.Factory<ForestGrowthData> factory = new SavedData.Factory<>(
-//$$            ForestGrowthData::new, 
-//$$            ForestGrowthData::load, 
-//$$            null
-//$$        );
-//$$        return level.getDataStorage().computeIfAbsent(factory, "physis_forest_data");
+//$$    private ForestGrowthData(ServerLevel level) {
+//$$        // Save data in the world folder to ensure stability across versions
+//$$        this.saveFile = level.getServer().getWorldPath(LevelResource.ROOT).resolve("physis_growth.dat").toFile();
+//$$        load();
 //$$    }
 //$$
-//$$    public ForestGrowthData() {}
+//$$    public static ForestGrowthData get(ServerLevel level) {
+//$$        return instances.computeIfAbsent(level, ForestGrowthData::new);
+//$$    }
 //$$
 //$$    public void addSapling(BlockPos pos) {
-//$$        modPlantedSaplings.add(pos.asLong());
-//$$        setDirty();
+//$$        if (modPlantedSaplings.add(pos.asLong())) {
+//$$            save();
+//$$        }
 //$$    }
 //$$
 //$$    public boolean isModPlanted(BlockPos pos) {
@@ -35,23 +40,33 @@ package org.gbxteam.physis;
 //$$
 //$$    public void removeSapling(BlockPos pos) {
 //$$        if (modPlantedSaplings.remove(pos.asLong())) {
-//$$            setDirty();
+//$$            save();
 //$$        }
 //$$    }
 //$$
-//$$    public static ForestGrowthData load(CompoundTag nbt, HolderLookup.Provider registries) {
-//$$        ForestGrowthData data = new ForestGrowthData();
-//$$        long[] array = nbt.getLongArray("saplings");
-//$$        for (long l : array) {
-//$$            data.modPlantedSaplings.add(l);
+//$$    private void load() {
+//$$        if (!saveFile.exists()) return;
+//$$        try {
+//$$            CompoundTag nbt = NbtIo.read(saveFile.toPath());
+//$$            if (nbt != null && nbt.contains("saplings")) {
+//$$                long[] array = nbt.getLongArray("saplings");
+//$$                for (long l : array) {
+//$$                    modPlantedSaplings.add(l);
+//$$                }
+//$$            }
+//$$        } catch (IOException e) {
+//$$            e.printStackTrace();
 //$$        }
-//$$        return data;
 //$$    }
 //$$
-//$$    @Override
-//$$    public CompoundTag save(CompoundTag nbt, HolderLookup.Provider registries) {
-//$$        nbt.putLongArray("saplings", modPlantedSaplings.stream().mapToLong(Long::longValue).toArray());
-//$$        return nbt;
+//$$    private void save() {
+//$$        try {
+//$$            CompoundTag nbt = new CompoundTag();
+//$$            nbt.putLongArray("saplings", modPlantedSaplings.stream().mapToLong(Long::longValue).toArray());
+//$$            NbtIo.write(nbt, saveFile.toPath());
+//$$        } catch (IOException e) {
+//$$            e.printStackTrace();
+//$$        }
 //$$    }
 //$$ }
 //#endif
