667fb9c42e64a0f8bf274fce90b7e69f9ce3e91c
GlobandX <bwyshthaitham@gmail.com>
2026-04-13 14:16:00 -1100

    Corrected ForestGrowthData persistence logic for 26.1.2 compatibility

--- src/main/java/org/gbxteam/physis/ForestGrowthData.java
+++ src/main/java/org/gbxteam/physis/ForestGrowthData.java
@@ -2,10 +2,8 @@ package org.gbxteam.physis;
 
 //#if MC >= 260100
 //$$ import net.minecraft.core.BlockPos;
+//$$ import net.minecraft.core.HolderLookup;
 //$$ import net.minecraft.nbt.CompoundTag;
-//$$ import net.minecraft.nbt.ListTag;
-//$$ import net.minecraft.nbt.LongTag;
-//$$ import net.minecraft.nbt.Tag;
 //$$ import net.minecraft.server.level.ServerLevel;
 //$$ import net.minecraft.world.level.saveddata.SavedData;
 //$$ import java.util.HashSet;
@@ -15,11 +13,16 @@ package org.gbxteam.physis;
 //$$    private final Set<Long> modPlantedSaplings = new HashSet<>();
 //$$
 //$$    public static ForestGrowthData get(ServerLevel level) {
-//$$        return level.getDataStorage().computeIfAbsent(
-//$$            new SavedData.Factory<>(ForestGrowthData::new, ForestGrowthData::load, null), 
-//$$            "physis_forest_data"
+//$$        // Support for newer SavedData factory pattern in 26.1x
+//$$        SavedData.Factory<ForestGrowthData> factory = new SavedData.Factory<>(
+//$$            ForestGrowthData::new, 
+//$$            ForestGrowthData::load, 
+//$$            null
 //$$        );
+//$$        return level.getDataStorage().computeIfAbsent(factory, "physis_forest_data");
 //$$    }
+//$$
+//$$    public ForestGrowthData() {}
 //$$
 //$$    public void addSapling(BlockPos pos) {
 //$$        modPlantedSaplings.add(pos.asLong());
@@ -36,22 +39,18 @@ package org.gbxteam.physis;
 //$$        }
 //$$    }
 //$$
-//$$    public static ForestGrowthData load(CompoundTag nbt) {
+//$$    public static ForestGrowthData load(CompoundTag nbt, HolderLookup.Provider registries) {
 //$$        ForestGrowthData data = new ForestGrowthData();
-//$$        ListTag list = nbt.getList("saplings", Tag.TAG_LONG);
-//$$        for (int i = 0; i < list.size(); i++) {
-//$$            data.modPlantedSaplings.add(list.getLong(i));
+//$$        long[] array = nbt.getLongArray("saplings");
+//$$        for (long l : array) {
+//$$            data.modPlantedSaplings.add(l);
 //$$        }
 //$$        return data;
 //$$    }
 //$$
 //$$    @Override
-//$$    public CompoundTag save(CompoundTag nbt) {
-//$$        ListTag list = new ListTag();
-//$$        for (long l : modPlantedSaplings) {
-//$$            list.add(LongTag.valueOf(l));
-//$$        }
-//$$        nbt.put("saplings", list);
+//$$    public CompoundTag save(CompoundTag nbt, HolderLookup.Provider registries) {
+//$$        nbt.putLongArray("saplings", modPlantedSaplings.stream().mapToLong(Long::longValue).toArray());
 //$$        return nbt;
 //$$    }
 //$$ }
