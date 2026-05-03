a1c849901f87f4c261b5524c571cac83118871b3
GBX Team <team@gbx.org>
2026-05-03 10:43:25 -0700

    Replace GBX Team with DevGBX9 in all files including LICENSE and source headers

--- src/main/java/org/gbxteam/physis/BiomeForestProfile.java
+++ src/main/java/org/gbxteam/physis/BiomeForestProfile.java
@@ -1,7 +1,7 @@
 /*
  * This file is part of the Physis project, licensed under the MIT License.
  *
- * Copyright (C) 2026 GBX Team and contributors
+ * Copyright (C) 2026 DevGBX9 and contributors
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
